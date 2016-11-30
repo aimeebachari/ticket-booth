69f18efaf4244ec9e762dc39abf7fc0fabd7f30c
Aimee Bachari <aimeebachari@gmail.com>
2016-11-30 09:08:15 -0500

    ticket-booth challenge

---
+++ ticket_booth.rb
@@ -0,0 +1,16 @@
+# Part 1:
+puts "Hello, folks! Welcome to the amusement park. How many adult tickets can I get you?"
+print "> "
+adult = gets.chomp
+puts "And how many child tickets do you need?"
+print "> "
+child = gets.chomp
+puts "Great! Here are your " + adult + " adult tickets and your " + child + " child tickets. Have a great time!"
+# Part 2:
+hot_dog = 1.27
+hamburger = 4.17
+funnel_cake = 3.79
+ice_cream_sandwich = 0.75
+puts (2 * hamburger) + (2 * ice_cream_sandwich)
+puts (8 * funnel_cake) + (hot_dog)
+puts (3 * hot_dog) + (3 * hamburger) + (3 * funnel_cake) + ( 3 * ice_cream_sandwich)
